8212dfad0ed03a27a6426bea2994af8eee7c3e18
Emilly12321 <emillysilvaluz2839@gmail.com>
2024-02-28 14:10:26 -0300

    ConsumidorApi

---
+++ src/componentes/PostsList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const PostsList = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Error fetching posts: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default PostsList;
